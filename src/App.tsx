8567df26084c780fc5b1e7bb2b573147a74de37e
LuisGustavo16 <guluisgustavo300@gmail.com>
2023-06-23 08:42:03 -0300

    a

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { GlobalStyle } from './styles/GlobalStyle'
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+        <h1>Mundo, mundo, vasto mundo <br />
+          Se eu me chamasse Raimundo <br />
+          Seria uma rima, não seria uma solução <br />
+          Mundo, mundo, vasto mundo</h1>
+      </>
+      )
+
+}
